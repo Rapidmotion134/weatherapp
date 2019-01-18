a13e6c88147ae1b3e42e767e5b454c55cbac04ee
Isaiah <ijshd7@mail.missouri.edu>
2019-01-18 11:27:19 -0600

    adding location detector

---
+++ script.js
@@ -0,0 +1,6 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    document.getElementById('data').innerHTML="latitude: "+ position.coords.latitude + "<br>longitude: " + position.coords.longitude;
+  });
+}
+
